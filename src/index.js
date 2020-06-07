153437f79c21975b1fdaa967c8838da248b79b94
Alex Eickelman <aeickelman40@gmail.com>
2020-06-07 12:54:37 -0600

    begin fetching data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/base.scss';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let allTravelersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
+  .then(response => response.json());
+
+let allTripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
+  .then(response => response.json());
+
+let allDestinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+  .then(response => response.json());
+
+let travelersData, tripsData, destinationsData;
+
+Promise.all([allTravelersData, allTripsData, allDestinationsData])
+  .then(data => {
+    travelersData = data[0];    
+    tripsData = data[1]; 
+    destinationsData = data[2];
+  })
+  .catch(error => {
+    console.error(error)
+  });
